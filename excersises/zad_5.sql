26c7364a8ed974f0cf5d7a4d7303cc51923376a3
Jakub Grobelny <72096815+kinsin5@users.noreply.github.com>
2024-10-07 15:52:17 +0200

    Create zad_5.sql

---
+++ excersises/zad_5.sql
@@ -0,0 +1,10 @@
+--1.
+INSERT INTO filmy (id_filmu, tytul, rok_produkcji, cena)
+VALUES 
+(11, 'Komornik', 2005, 10.5);
+--SELECT * FROM filmy;
+--2.
+DELETE FROM filmy 
+WHERE rok_produkcji = 2005;
+--SELECT * FROM filmy;
+--3.
