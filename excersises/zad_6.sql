48d923e68355a8a1da8a04b4ef371321901e6934
Jakub Grobelny <jakobgrob9@gmail.com>
2024-10-08 22:25:24 +0200

    Update zad_6.sql

--- excersises/zad_6.sql
+++ excersises/zad_6.sql
@@ -24,4 +24,24 @@ SELECT
 WHERE TABLE_NAME = 'ksiazki';
 --5.
 DROP TABLE ksiazki;
+DROP TABLE osoby;
+--6.
+CREATE TABLE osoby (
+	PESEL CHAR(11) PRIMARY KEY CHECK (LENGTH(PESEL) = 11),
+	IMIE VARCHAR(15) NOT NULL,
+	NAZWISKO VARCHAR(15) NOT NULL,
+	DATA_URODZENIA DATE,
+	CHECK (SUBSTRING((EXTRACT(YEAR FROM data_urodzenia))::varchar,3,2) = LEFT(pesel, 2)),
+	CHECK (SUBSTRING(data_urodzenia::varchar,6,2) = SUBSTRING(pesel, 3, 2)),
+	CHECK (SUBSTRING(data_urodzenia::varchar,9,2) = SUBSTRING(pesel, 5, 2))
+);
+--7.
+INSERT INTO osoby (pesel, imie, nazwisko, data_urodzenia)
+VALUES
+('39090100001','Jan', 'Kowalski', '1939-09-01' ),
+('750218','Adam','Nowak','1975-03-18'),	--pesel LENGTH, month != pesel
+('75021800123','Adam','Nowak','1975-02-20'), -- day != pesel
+('75021800123','Adam','Nowak','1975-02-18') -- not unique pesel,
+--8.
+
 
